269d52038ed0c6d7382c00a5e1effeb3aee62492
Paul Francis <paul@francis.com>
2021-01-28 08:42:41 +0100

    added methods to read various individual parameters diffixRef.py

--- diffixRef.py
+++ diffixRef.py
@@ -1,6 +1,7 @@
 import pprint
 import subprocess
 import csv
+import json
 
 class diffixRef:
     """
@@ -14,18 +15,66 @@ class diffixRef:
                 ):
         self.cmd = cmd
         self.db = db
+        rtn = self.getParams()
+        if rtn['success']:
+            self.params = rtn['params']
+        else:
+            print(f"ERROR diffixRef: bad getParams: {rtn['error']}")
+            quit()
         self.outliers = outliers
         self.top = top
         self.lcf = lcf
         self.noise = noise
+        if self.outliers:
+            self.params['outlier_count']['lower'] = outliers[0]
+            self.params['outlier_count']['upper'] = outliers[1]
+        if self.top:
+            self.params['top_count']['lower'] = top[0]
+            self.params['top_count']['upper'] = top[1]
+        if self.noise:
+            self.params['noise']['standard_dev'] = noise[0]
+            self.params['noise']['cutoff'] = noise[1]
 
-    def query(self,sql,seed=1,aids=[{'table':'tab','aid':'aid1'}]):
-        pp = pprint.PrettyPrinter(indent=4)
+    def getNoiseParams(self):
+        return self.params['noise']
+
+    def getOutlierParams(self):
+        return self.params['outlier_count']
+
+    def getTopParams(self):
+        return self.params['top_count']
+
+    def _getBasicCommands(self):
         command = []
         command.append(self.cmd)
         # database
         command.append('-d')
         command.append(self.db)
+        return command
+
+    def getParams(self):
+        command = self._getBasicCommands()
+        command.append('--aid-columns')
+        command.append('a.b')
+        # query
+        command.append('-q')
+        #command.append('select col from tab')
+        command.append('anything')
+        command.append('--dry-run')
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+        rtn = {}
+        out = result.stdout.decode("utf-8")
+        if result.returncode != 0:
+            rtn['success'] = False
+            rtn['error'] = out
+        else:
+            rtn['success'] = True
+            params = json.loads(out)
+            rtn['params'] = params['anonymization_parameters']
+        return rtn
+
+    def query(self,sql,seed=1,aids=[{'table':'tab','aid':'aid1'}]):
+        command = self._getBasicCommands()
         # aid columns
         command.append('--aid-columns')
         for tabAid in aids:
@@ -54,26 +103,41 @@ class diffixRef:
             command.append(str(self.noise[0]))  # stddev
             command.append(str(self.noise[1]))  # cutof
         result = subprocess.run(command, stdout=subprocess.PIPE)
-        if result.returncode != 0:
-            return None
-        # Turn result bytes into a string
+        rtn = {}
         out = result.stdout.decode("utf-8")
-        outList = str.splitlines(out)
-        reader = csv.reader(iter(outList),delimiter=';')
-        data = list(reader)
-        return data
+        if result.returncode != 0:
+            rtn['success'] = False
+            rtn['error'] = out
+        else:
+            rtn['success'] = True
+            # Turn result bytes into a string
+            outList = str.splitlines(out)
+            reader = csv.reader(iter(outList),delimiter=';')
+            rtn['answer'] = list(reader)
+        return rtn
         
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
     dr = diffixRef()
-    data = dr.query("select t1 from tab")
-    if data:
-        pp.pprint(data)
+    rtn = dr.query("select t1 from tab")
+    if rtn['success']:
+        pp.pprint(rtn['answer'])
     else:
-        print("Query in error")
-    data = dr.query("select count(*) from tab")
-    if data:
-        pp.pprint(data)
+        print(f"ERROR: {rtn['error']}")
+    rtn = dr.query("select count(*) from tab")
+    if rtn['success']:
+        pp.pprint(rtn['answer'])
     else:
-        print("Query in error")
+        print(f"ERROR: {rtn['error']}")
+    rtn = dr.getParams()
+    if rtn['success']:
+        pp.pprint(rtn['params'])
+    else:
+        print(f"ERROR: {rtn['error']}")
+    print("Noise")
+    pp.pprint(dr.getNoiseParams())
+    print("Outliers")
+    pp.pprint(dr.getOutlierParams())
+    print("Top")
+    pp.pprint(dr.getTopParams())
