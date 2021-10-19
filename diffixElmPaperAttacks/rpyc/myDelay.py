f75672f16f89b1573bfe67d10c473c953a70f35f
Paul Francis <paul@francis.com>
2021-10-19 10:36:22 +0200

    test

--- diffixElmPaperAttacks/rpyc/myDelay.py
+++ diffixElmPaperAttacks/rpyc/myDelay.py
@@ -4,8 +4,8 @@ class test():
     def __init__(self):
         pass
 
-    def delay(self,delay): # this is an exposed method
-        time.sleep(delay)
+    def delay(self,delay):
+        time.sleep(10)
         return delay+1
 
 if __name__ == "__main__":
