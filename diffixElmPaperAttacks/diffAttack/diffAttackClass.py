28e3316f53ceec22daa6dab23014bce71367eaab
Paul Francis <paul@francis.com>
2021-10-20 17:08:45 +0200

    Update diffAttackClass.py

--- diffixElmPaperAttacks/diffAttack/diffAttackClass.py
+++ diffixElmPaperAttacks/diffAttack/diffAttackClass.py
@@ -166,8 +166,7 @@ class diffAttack():
         while True:
             numTries += 1
             claimCorrect,difference = self.runOne(params,numTries)
-            #if numTries % 1000 == 1 and self.doLog:
-            if self.doLog:
+            if numTries % 1000 == 1 and self.doLog:
                 self.f.write(f"    tries {numTries}\n")
                 self.f.flush()
             #---------------------------------------------------------------------------------
